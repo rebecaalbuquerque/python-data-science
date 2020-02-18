0040761f050f663660534f242c848c8f7fb96770
rebecaalbuquerque <rebeca.albuquerque96@gmail.com>
2020-02-17 23:16:18 -0300

    Finalizando função que gera rows de um json descomprimido

--- other/main.py
+++ other/main.py
@@ -5,6 +5,7 @@ from collections import defaultdict
 data = json.load(open("data/teste.json"))
 df = pd.DataFrame()
 
+
 def decompression_dictionary(colunmns, dictionary, prefix=None):
     """
     Percorre um objeto de json recursivamente e transforma todos os outros objetos e lista de objetos desse json
@@ -32,20 +33,35 @@ def decompression_dictionary(colunmns, dictionary, prefix=None):
             else:
                 colunmns.append((prefix + "_" + key, value))
 
-# todo: não gerar o DF e sim as rows do DF
+
 def generate_rows(columns):
     grouped_list = defaultdict(list)
 
     for k, v in columns:
         grouped_list[k].append(v)
 
-    df = pd.DataFrame(columns=list(grouped_list.keys()))
-    row = [[]]
+    rows = [[] for i in range(1)]
 
-    for index, key in enumerate(grouped_list.items()):
-        index
+    for key, value in grouped_list.items():
+
+        for i in range(len(value)):
+
+            if i in range(len(rows)):
+                rows[i].append(value[i])
+            else:
+                rows.append([])
+                rows[i].append(value[i])
+
+    for i in range(len(rows)):
+        if len(rows[i]) < len(max(rows, key=len)):
+            n = len(max(rows, key=len)) - (len(rows[i]))
+            rows[i] = [None]*n + rows[i]
+
+    return list(grouped_list.keys()), rows
+
 
 columns_value = []
 decompression_dictionary(columns_value, data)
 
-generate_rows(columns_value)
+list_columns, list_rows = generate_rows(columns_value)
+df = pd.DataFrame(list_rows, columns=list_columns)
